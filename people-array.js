ab802c6d00fdb464e3b623f76a80fe5ef90dfe5e
Your Name <you@example.com>
2025-10-27 11:24:47 +0200

    peopleArray

---
+++ people-array.js
@@ -0,0 +1,9 @@
+let people = {
+  name: ("jurģis", "oskars", "gatis", "haralds"),
+  age: (11, 15),
+  isStudent: (false, true),
+};
+let i = people.name.length;
+for (let i = 0; i < 3; i++) {
+  console.log("Vārds: " + people.name[i]);
+}
