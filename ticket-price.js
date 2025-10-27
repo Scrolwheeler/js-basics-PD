60a91b36c661e5c419a3330114f766948d8734ef
Your Name <you@example.com>
2025-10-27 11:09:24 +0200

    Pabeidzu ticketPrice uzd

---
+++ ticket-price.js
@@ -0,0 +1,11 @@
+let visitorAge = 12;
+let isStudent = true;
+
+if (visitorAge >= 6) {
+  console.log("ieeja bez maksas");
+}
+if ((isStudent = true)) {
+  console.log("biļete maksā 5eur");
+} else {
+    console.log("biļete maksā 10eur")
+}
